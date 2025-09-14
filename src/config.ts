c4d00b3538305d9fbae97c508d742ed14a33b5e3
Hajo Haas <hajo.haas@hmmh.de>
2025-09-14 16:33:52 +0200

    feat: add database config

---
+++ src/config.ts
@@ -0,0 +1,56 @@
+import z from "zod";
+import fs from "fs";
+
+export type Config = z.infer<typeof ConfigSchema>;
+
+const ConfigSchema = z.object({
+  db_url: z.string(),
+  current_user_name: z.string().optional(),
+});
+
+export function setUser(userName: string) {
+  try {
+    const cfg = readConfig();
+    cfg.current_user_name = userName;
+    writeConfig(cfg);
+  } catch (err) {
+    throw err;
+  }
+}
+
+export function readConfig(): Config {
+  const rawConfig = fs.readFileSync(getConfigFilePath(), { encoding: "utf-8" });
+
+  const config = validateConfig(rawConfig);
+
+  if (!config) {
+    throw new Error("invalid config");
+  }
+
+  return config;
+}
+
+function writeConfig(cfg: Config): void {
+  fs.writeFileSync(getConfigFilePath(), JSON.stringify(cfg), {
+    encoding: "utf-8",
+  });
+}
+
+function validateConfig(rawConfig: any) {
+  const parsed = JSON.parse(rawConfig);
+
+  try {
+    const config = ConfigSchema.parse(parsed);
+    return config;
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.error("validation error", err);
+    } else {
+      throw err;
+    }
+  }
+}
+
+function getConfigFilePath() {
+  return "./.gatorconfig.json";
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { readConfig, setUser } from "./config";
+
 function main() {
-  console.log("Hello, world!");
+  setUser("Roq");
+
+  const cfg = readConfig();
+
+  console.log("dbUrl:", cfg.db_url);
+  console.log("currentUserName:", cfg.current_user_name);
 }
 
 main();
